173565397bc5960d09b5e5af510c734453360938
Johannes Bülow <johannes.buelow@jmbit.de>
2025-03-19 09:03:48 +0100

    added header

--- internal/middlewares/headers.go
+++ internal/middlewares/headers.go
@@ -8,6 +8,7 @@ func ServerHeaders(next http.Handler) http.Handler {
 			w.Header().Set("Server", "Microsoft-IIS/5.1")
 			w.Header().Set("Cache-Control", "private")
       w.Header().Set("Set-Cookie", "ASPSESSIONIDSARQSTTR=HAKPENADIDKHMGENJFPENNDM; path=/")
+      w.Header().Set("X-Powered-By", "ASP.NET")
 		next.ServeHTTP(w, r)
 	})
 }
